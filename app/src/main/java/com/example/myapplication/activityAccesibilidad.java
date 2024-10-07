a501d9442461fa63c70e8d4ca6757fdbf3888402
Alejandro González <andresg.vergara04@gmail.com>
2024-10-07 20:25:27 -0300

    Version 3.0.4: Se logró hacer que funcione la accesibilidad de día y noche, sin embargo, por el momento hay problemas con el mainActivity ya que es el único que no cambia.

--- app/src/main/java/com/example/myapplication/activityAccesibilidad.java
+++ app/src/main/java/com/example/myapplication/activityAccesibilidad.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,14 +19,21 @@ import android.widget.Spinner;
 
 public class activityAccesibilidad extends AppCompatActivity {
 
-    private Button btnNoche, btnDia, btnAumentar, btnReducir;
-    private Spinner spinnerSensitivity;
     private SharedPreferences sharedPreferences;
+    private static final String PREFS_NAME = "themePrefs";
+    private static final String THEME_KEY = "theme";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String currentTheme = sharedPreferences.getString(THEME_KEY, "day");
+        if (currentTheme.equals("night")){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
         setContentView(R.layout.activity_accesibilidad);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -33,57 +41,32 @@ public class activityAccesibilidad extends AppCompatActivity {
             return insets;
         });
 
-        sharedPreferences = getSharedPreferences("AccesibilityPrefs", MODE_PRIVATE);
-        btnNoche = findViewById(R.id.btnNoche);
-        btnDia = findViewById(R.id.btnDia);
-        btnAumentar = findViewById(R.id.btnAumentar);
-        btnReducir = findViewById(R.id.btnReducir);
-        spinnerSensitivity = findViewById(R.id.spinner);
+        Button btnNoche = findViewById(R.id.btnNoche);
+        Button btnDia = findViewById(R.id.btnDia);
 
-        btnNoche.setOnClickListener(view -> setNightMode());
-        btnDia.setOnClickListener(view -> setDayMode());
-        btnAumentar.setOnClickListener(view -> changeTextSize(true));
-        btnReducir.setOnClickListener(view -> changeTextSize(false));
-
-        spinnerSensitivity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String sensitivityLevel = (String) parent.getItemAtPosition(position);
-                sharedPreferences.edit().putString("sensitivity", sensitivityLevel).apply();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+        btnNoche.setOnClickListener(v -> setAppTheme("night"));
+        btnDia.setOnClickListener(v -> setAppTheme("day"));
     }
-    private void setNightMode(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.black, getTheme()));
+
+    public void setAppTheme(String theme){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(THEME_KEY, theme);
+        editor.apply();
+
+        if (theme.equals("night")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         } else {
-            getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.black));
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
+        recreate();
     }
-    private void setDayMode(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.white, getTheme()));
-        } else {
-            getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.black));
-        }
-    }
-    public void changeTextSize(boolean increase){
-        float currentSize = getResources().getConfiguration().fontScale;
-        float newSize = increase ? currentSize + 0.1f : currentSize - 0.1f;
-        if (newSize >= 1.0f && newSize <= 1.5f){
-            sharedPreferences.edit().putFloat("textSize", newSize).apply();
-            getResources().getConfiguration().fontScale = newSize;
-            getResources().updateConfiguration(getResources().getConfiguration(), getResources().getDisplayMetrics());
-        }
-    }
+
+
 
     public void paginaMenuPrincipal(View v){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
 }
